c823ff51aab6ea89733c3e1d6b39bf48ae5ab8c8
Fayaz-Ahmed <fayaz861818@gmail.com>
2021-06-17 01:10:41 +0530

    complete user interface

---
+++ adminboard/src/components/widgetLarge/widgetLarge.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import "./widgetLarge.css"
+
+function WidgetLarge() {
+    const Button = ({type}) => {
+        return <button className={"widgetLargeButton " + type}>{type}</button>
+    }
+    return (
+        <div className="widgetLarge">
+            <h3 className="widgetLargeTitle">Current transactions</h3>
+            <table className="widgetLargeTable">
+                <tr className="widgetLargeTr">
+                    <th className="widgetLargeTh">User</th>
+                    <th className="widgetLargeTh">Date</th>
+                    <th className="widgetLargeTh">Amount</th>
+                    <th className="widgetLargeTh">Status</th>
+                </tr>
+                <tr className="widgetLargeTr">
+                    <td className="widgetLargeUser">
+                        <img src="../assets/images/image.jpg" alt="" className="widgetLargeImg"/>
+                        <span className="widgetLargeUsername">Nutra Thawed</span>
+                    </td>
+                    <td className="widgetLargeDate">21/06/2021</td>
+                    <td className="widgetLargeAmount">$122.00</td>
+                    <td className="widgetLargeStatus"><Button type="Approved"/></td>
+                </tr>
+                <tr className="widgetLargeTr">
+                    <td className="widgetLargeUser">
+                        <img src="../assets/images/image.jpg" alt="" className="widgetLargeImg"/>
+                        <span className="widgetLargeUsername">Nutra Thawed</span>
+                    </td>
+                    <td className="widgetLargeDate">21/06/2021</td>
+                    <td className="widgetLargeAmount">$122.00</td>
+                    <td className="widgetLargeStatus"><Button type="Declined"/></td>
+                </tr>
+                <tr className="widgetLargeTr">
+                    <td className="widgetLargeUser">
+                        <img src="../assets/images/image.jpg" alt="" className="widgetLargeImg"/>
+                        <span className="widgetLargeUsername">Nutra Thawed</span>
+                    </td>
+                    <td className="widgetLargeDate">21/06/2021</td>
+                    <td className="widgetLargeAmount">$122.00</td>
+                    <td className="widgetLargeStatus"><Button type="Pending"/></td>
+                </tr>
+            </table>
+        </div>
+    );
+}
+
+export default WidgetLarge;
